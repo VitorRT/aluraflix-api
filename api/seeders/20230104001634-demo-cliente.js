073e64022c24d3fd91a206c0d4125cdf4e75aa8a
VitorRT <barberino.italia@gmail.com>
2023-01-03 23:25:07 -0300

    update: criando seed para clientes

--- api/seeders/20230104001634-demo-cliente.js
+++ api/seeders/20230104001634-demo-cliente.js
@@ -9,7 +9,42 @@ module.exports = {
           nome: "Victor Barberino",
           email: "vitu.barberino@gmail.com",
           senha: "adm123"
-      }
+        },
+        {
+          nome: "Alexandre P. Sobral",
+          email: "alexandre.psobral@gmail.com",
+          senha: 'tico123'
+        },
+        {
+          nome: "Sara alcantara",
+          email: "saraarh.alcantara@gmail.com",
+          senha: "mylover123"
+        },
+        {
+          nome: "Luis Reis",
+          email: "luisgrande.rei@gmail.com",
+          senha: "batatinhafrita1234"
+        },
+        {
+          nome: "Raquel Neves",
+          email: "raquellinda@gmail.com",
+          senha: "meuvivis2"
+        },
+        {
+          nome: "Pablo Carlos",
+          email: "pabloo.manito@gmail.com",
+          senha: "craftdes123@sd"
+        },
+        {
+          nome: "Pedro Moura",
+          email: "pedrorodrigues.moura@gmail.com",
+          senha: "sevenpecaids@best"
+        },
+        {
+          nome: "Mirela S2",
+          email: "taigaa.mih@gmail.com",
+          senha: "mr.taiga@potato"
+        }
     ], {});
 
   },
